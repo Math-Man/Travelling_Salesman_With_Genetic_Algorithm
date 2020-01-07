a19ce5daceba1e85685eb1ef8fc79d5e40d87309
Math-Man <kayacan.goktug@gmail.com>
2020-01-07 13:04:58 +0300

    working

--- src/GeneticAlgorithm.py
+++ src/GeneticAlgorithm.py
@@ -30,16 +30,28 @@ class GeneticAlgorithm:
 #new generation is built by the consecutive events of selection->crossover->mutation for each agent
 
     ##PARAMETERS##
-    MAX_GENERATION = 9999;
-    POPULATION_SIZE = 25;
-    MUTATION_RATE = 0.05;
+    POPULATION_SIZE = 100;
+    MUTATION_RATE = 0.1;
     MUTATION_STRENGTH = 0.05;
     CROSSOVER_CHANCE = 0.5;
-    ##PARAMETERS##
+    ##PARAMETERS OVER##
+
+
+
+
+
+
+
+
 
     rawData = [];
     currentGeneration = [];
     newGeneration = [];
+
+    generationIndex = 0;
+
+    bestInGenerations = [];
+    bestEver = None;
 
 
     #random population
@@ -47,6 +59,8 @@ class GeneticAlgorithm:
         agentBuilder = RandomAgentBuilder();
         for i in range(self.POPULATION_SIZE):
             self.currentGeneration[i] = agentBuilder.randomAgent(len(self.rawData)); #Use agent builder to build random agents
+            self.currentGeneration[i].calculateScore(self.rawData);
+
         return;
 
 
@@ -54,27 +68,13 @@ class GeneticAlgorithm:
     def BuildNextGeneration(self):
         listOfPairs = self.selection();
         self.crossOver(listOfPairs);
-        self.mutation();
+        self.mutateGeneration();
         return;
 
     #weighted random selection alghorithm depending on the score of the agents
     def selection(self):
         #Calculate scores
-        scoreSum = 0;
-        scores = [0]*self.POPULATION_SIZE;
-        for i in range(0,self.POPULATION_SIZE):
-            scores[i] = self.currentGeneration[i].calculateScore(self.rawData);
-            scoreSum = scoreSum + scores[i];
-        #translate values to range between: (0,POPULATION_SIZE/100)
-        currentMax = max(scores);
-        currentMin = min(scores);
-
-        summ = 0;
-        for i in range(0,self.POPULATION_SIZE):
-            scores[i] = (scores[i]/scoreSum) * 100;
-            summ = summ + scores[i];
-        #add division fault to first score to make the selection algorithm work properly (usually has very low significance has no effect on algorithm)
-        scores[0] = scores[0] + (100 - summ);
+        scores = self.weightedScoreCalculation();
 
         #apply weighted random selection, create a list of pairs and return it
         listOfPairs = [0]*self.POPULATION_SIZE;
@@ -105,7 +105,24 @@ class GeneticAlgorithm:
             total = total + currentItem;
         return -1;
 
+    def weightedScoreCalculation(self):
+        #Calculate scores
+        scoreSum = 0;
+        scores = [0]*self.POPULATION_SIZE;
+        for i in range(0,self.POPULATION_SIZE):
+            scores[i] = self.currentGeneration[i].calculateScore(self.rawData);
+            scoreSum = scoreSum + scores[i];
+        #translate values to range between: (0,POPULATION_SIZE/100)
+        currentMax = max(scores);
+        currentMin = min(scores);
 
+        summ = 0;
+        for i in range(0,self.POPULATION_SIZE):
+            scores[i] = (scores[i]/scoreSum) * 100;
+            summ = summ + scores[i];
+        #add division fault to first score to make the selection algorithm work properly (usually has very low significance has no effect on algorithm)
+        scores[0] = scores[0] + (100 - summ);
+        return scores;
 
 
     def crossOver(self, listOfPairs):
@@ -118,34 +135,146 @@ class GeneticAlgorithm:
             parent2 = listOfPairs[i][1];
             DNALength = len(parent1.localeSequence);
 
-            print(parent1.localeSequence);
-            print(parent2.localeSequence);
+            #print(parent1.localeSequence);
+            #print(parent2.localeSequence);
             #create child agent
             child = Agent(DNALength);
-            child.localeSequence = parent1.localeSequence;
+            child.localeSequence = [-1]*len(parent1.localeSequence); #parent1.localeSequence;
 
             #apply corssover
+            #pick values from each parent and copy their locations to the child
+            fromfirstParent = [];
+            fromSecondParent = [];
             for k in range(DNALength):
-                if(random.random() >= self.CROSSOVER_CHANCE):
+                if(random.random() <= self.CROSSOVER_CHANCE):
+                    fromfirstParent.append(k);
                     #pick from the sequence of second parent
-                    #print("crossOver "+ str(k));
-                    child.localeSequence[k] = parent2.localeSequence[k];
-                #print("NO crossOver " + str(k));
+                    #item = parent2.localeSequence[k]
+                    #child.localeSequence[k] = item;
+                else:
+                    fromSecondParent.append(k);
+                    #item = parent1.localeSequence[k]
+                    #child.localeSequence[k] = item;
+                    #print("NO crossOver " + str(k));
+
+                #start pulling from parents and add to child
+
+            #print("parents")
+            #print(fromfirstParent)
+            #print(fromSecondParent)
+
+            for j in range(0,DNALength):
+                if j in fromfirstParent:
+                    locIndex = parent1.localeSequence[j]; #where the location j is in the parent's sequence
+                    #print("first: " + str(locIndex));
+                    child.localeSequence[parent1.localeSequence.index(j)] = j;
+
+                elif j in fromSecondParent:
+                    locIndex = parent2.localeSequence[j]; #where the location j is in the parent's sequence
+                    #print("second: " + str(locIndex));
+                    child.localeSequence[parent1.localeSequence.index(j)] = j;
 
             #add new child to newGenerations list
             self.newGeneration[i] = child;
-            print(child.localeSequence);
+            #print(child.localeSequence);
+        #print("Crossover complete");
         return self.newGeneration;
 
-
-    def mutation(self):
+    def mutateGeneration(self):
+        for i in range(0, len(self.newGeneration)):
+            self.newGeneration[i] = self.mutate(self.newGeneration[i]);
+            #print(self.newGeneration[i].localeSequence);
+        #print("mutations complete");
         return;
 
+    #swap ceiling(mutationstrength) number of items randomly
+    def mutate(self, agent):
+        sequenceLength = len(agent.localeSequence);
+        numberOfMutations = math.ceil(sequenceLength * self.MUTATION_STRENGTH);
+        #print(agent.localeSequence)
+        if random.random() < self.MUTATION_RATE: #If mutation chance check passes
+            for i in range(0, numberOfMutations):
+                #select a random index
+                randomIndex = random.randint(0,sequenceLength-1);
+                randomSwapIndex = random.randint(0,sequenceLength-1);
+                agent.localeSequence[randomIndex] ,  agent.localeSequence[randomSwapIndex] = agent.localeSequence[randomSwapIndex] ,  agent.localeSequence[randomIndex]
+                #print(agent.localeSequence)
+        agent.calculateScore(self.rawData);
+        return agent;
 
+
+
+
+    #sets bests and replaces current generation
     def replaceCurrentGeneration(self):
-        self.currentGeneration = self.newGeneration;
+
+
+        #set scores
+        self.bestInGenerations.append(self.getBestInCurrentGeneration());
+        self.bestEver = self.getBestEver();
+
+        #print("best now: SCORE:" + str(self.getBestInCurrentGeneration().score) + " TOTAL DISTANCE: " + str(self.getBestInCurrentGeneration().distanceTravelled));
+        #print("best ever: " + str(self.getBestEver().score )  + " TOTAL DISTANCE: " + str(self.getBestEver().distanceTravelled));
+
+        #replace
+
+        self.currentGeneration = self.newGeneration.copy();
         self.newGeneration = [0]*self.POPULATION_SIZE;
+
+        self.generationIndex = self.generationIndex + 1;
+
         return;
+
+
+
+    def step(self):
+        print("Processing Next Generation...");
+        self.BuildNextGeneration();
+        self.replaceCurrentGeneration();
+
+
+
+
+
+    #functions to get best and avrg in both generations
+    def getBestInCurrentGeneration(self):
+        best = -1;
+        bestAgent = None;
+        for agent in self.currentGeneration:
+            if agent.score > best:
+                best = agent.score;
+                bestAgent = agent;
+        return bestAgent;
+
+    def getBestInNewGeneration(self):
+        best = -1;
+        bestAgent = None;
+        for agent in self.newGeneration:
+            if agent.score > best:
+                best = agent.score;
+                bestAgent = agent;
+        return bestAgent;
+
+    def getBestEver(self):
+        best = -1;
+        bestAgent = None;
+        for agent in self.bestInGenerations:
+            if agent.score > best:
+                best = agent.score;
+                bestAgent = agent;
+        return bestAgent;
+
+    def getCurrentGenerationAverage(self):
+        sum = 0;
+        for agent in self.currentGeneration:
+            sum = sum + agent.score;
+        return sum/self.POPULATION_SIZE;
+
+    def getNewGenerationAverage(self):
+        sum = 0;
+        for agent in self.newGeneration:
+            sum = sum + agent.score;
+        return sum/self.POPULATION_SIZE;
 
     #entry
     def __init__(self, data):
@@ -153,5 +282,3 @@ class GeneticAlgorithm:
         self.currentGeneration = [0]*self.POPULATION_SIZE; #list with size POPULATION_SIZE
         self.newGeneration = [0]*self.POPULATION_SIZE; #list with size POPULATION_SIZE
         self.rawData = data;
-
-        self.buildInitialPopulation();
--- src/Main.py
+++ src/Main.py
@@ -1,18 +1,90 @@
 from GeneticAlgorithm import GeneticAlgorithm;
 import csv;
-
+import os;
+clear = lambda: os.system('cls')
 
 def main():
-    data = csvReader("Data1.csv");
+
+    #menu
+    clear();
+    print("Change parameters under (GeneticAlgorithm.py) to custimize the algorithm");
+    print("Select Dataset (Enter: 1, 2, 3)");
+    inDataset = input();
+    dataSet = csvReader("Data" + str(inDataset) + ".csv");
+    geneticAlgMenuLoop(dataSet);
     #print(getDistanceFor(1, 10, data));
-    genAlg = GeneticAlgorithm(data);
-    genAlg.BuildNextGeneration();
-
-#menu loop function
-def menu():
-    print("Select Dataset... etc");
 
 
+
+
+
+
+    #genAlg.BuildNextGeneration();
+
+def geneticAlgMenuLoop(dataset):
+    exit = 0;
+
+
+    clear();
+    print("Starting genetic algorithm, enter (exit) anytime to return to dataset selection.\n");
+    genAlg = GeneticAlgorithm(dataset);
+    genAlg.buildInitialPopulation();
+
+    print("Best in initial population:");
+    printCurrentBests(genAlg);
+
+
+    clear();
+    while exit == 0:
+
+        print("Current Generation: " + str(genAlg.generationIndex));
+        printCurrentBests(genAlg);
+        printHelpMenu();
+        InMenu = input();
+        clear();
+        if InMenu == 'step':
+            #genAlg.step();
+            genAlg.BuildNextGeneration();
+            #printCurrentBests(genAlg);
+            genAlg.replaceCurrentGeneration();
+
+        elif InMenu == 'run':
+            clear();
+            print("Enter number of generations to process");
+            InNum = int(input());
+            for i in range(0,InNum):
+                genAlg.BuildNextGeneration();
+                #printCurrentBests(genAlg);
+                genAlg.replaceCurrentGeneration();
+
+        elif InMenu == 'bests':
+            for i in range(0, len(bestInGenerations)):
+                print("Score: " + str( genAlg.bestInGenerations[i].score));
+                print("Distance Travelled: " +  str(genAlg.bestInGenerations[i].distanceTravelled));
+                print("Sequence: " +  str(genAlg.bestInGenerations[i].localeSequence));
+
+        elif InMenu == 'best ever':
+            print("Score: " + str( genAlg.bestEver.score));
+            print("Distance Travelled: " +  str(genAlg.bestEver.distanceTravelled));
+            print("Sequence: " +  str(genAlg.bestEver.localeSequence));
+
+
+        elif InMenu == 'exit':
+            exit = 1;
+
+
+def printHelpMenu():
+    print("\nType (step) to process a single generation");
+    print("Type (run) and enter a number to process ~x amount of generations");
+    print("Type (bests) to show sequences and information of the bests in all processed generations");
+    print("Type (best ever) to show sequence and information of the best ever agent");
+    print("Type (exit) anytime to return to dataset selection")
+
+def printCurrentBests(genAlg):
+    print("\nScore: " + str( genAlg.getBestInCurrentGeneration().score));
+    print("Distance Travelled: " +  str(genAlg.getBestInCurrentGeneration().distanceTravelled));
+    print("Sequence: " +  str(genAlg.getBestInCurrentGeneration().localeSequence));
+    print("Current Generation Average Score:" + str(genAlg.getCurrentGenerationAverage()));
 #reads data file (converted to .csv file)
 #data file should be outside src folder within a data folder
 def csvReader(fileName):
